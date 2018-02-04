c6edd4e1440a13e0d4ca4288dd598ee41b059184
Bill Marcy <william_marcy@brown.edu>
2018-02-04 13:35:02 -0500

    made testing into two commands

--- TestingModel.cs
+++ TestingModel.cs
@@ -23,17 +23,8 @@ public class Testing {
         System.Console.WriteLine("====================");
     }
 
-    static void TestLogicalForms() {
-        System.Console.WriteLine("=====================");
-        System.Console.WriteLine("Testing LogicalForms:");
-        Constant c = new Constant(new E(), 4);
-        System.Console.WriteLine(c);
-        System.Console.WriteLine("=====================");
-    }
-
     static void Main() {
         System.Console.WriteLine("TESTING MODEL");
         TestTruthValues();
-        TestLogicalForms();
     }
 }
--- TestingProof.cs
+++ TestingProof.cs
@@ -1,27 +1,6 @@
 using System;
 
 public class Testing {
-    static void TestTruthValues() {
-        System.Console.WriteLine("====================");
-        System.Console.WriteLine("Testing TruthValues:");
-        TruthValue v = new TruthValue();
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(true));
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(true));
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(false));
-        System.Console.WriteLine(v.toString());
-        v.Clear();
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(false));
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(false));
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine(v.Add(true));
-        System.Console.WriteLine(v.toString());
-        System.Console.WriteLine("====================");
-    }
 
     static void TestLogicalForms() {
         System.Console.WriteLine("=====================");
@@ -33,7 +12,6 @@ public class Testing {
 
     static void Main() {
         System.Console.WriteLine("TESTING PROOF");
-        TestTruthValues();
         TestLogicalForms();
     }
 }
